6a29266ad83f4250fea0ec25617dce864f6c6413
misland <guanghui1136@126.com>
2019-10-30 11:41:52 +0800

    测试原生js

---
+++ js/amazing.js
@@ -0,0 +1,17 @@
+/*
+ * @Description: 
+ * @Author: Loki Zhao
+ * @Copyright: Karl Storz
+ * @Date: 2019-10-30 11:24:09
+ * @LastEditors: Loki Zhao
+ * @LastEditTime: 2019-10-30 11:26:30
+ */
+// 向下取整
+console.log(~~2.093);
+
+console.log(~~Math.PI);
+
+console.log(Math.E.toFixed(3));
+
+//快速初始化一个数组
+console.log(Array(5).fill(0));
